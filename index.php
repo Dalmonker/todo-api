c8e18645e6b4b23eeb870c672bc53839564b9861
Dalmonker <gavrilovichdmitry1@gmail.com>
2026-01-27 20:14:28 +0300

    +

--- index.php
+++ index.php
@@ -2,3 +2,16 @@
 
 session_start();
 
+require_once 'config.php';
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (isset($_GET['act'])) {
+    switch ($_GET['act']) {
+        case 'login':
+            require_once 'action/login.php';
+            break;
+    }
+    die;
+}
+
